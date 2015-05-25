7f181992187ca1541e49bc39eb547fbca5e6d80a
Josh Wykell <jwykell@gmail.com>
2015-05-24 20:47:54 -0400

    2 working

--- green_eggs_and_ham.rb
+++ green_eggs_and_ham.rb
@@ -1,6 +1,21 @@
+
 class GreenEggsAndHam
+  include Enumerable
 
   def initialize
     @text = File.read('green_eggs_and_ham.txt')
   end
-end
+
+  def words
+    @text.split.map!{|word| word.downcase.gsub(/[^a-z-]/, '')}
+  end
+
+  def word_count
+    words.count
+  end
+
+  def sorted_unique_words
+    words.uniq.sort
+  end
+
+end    
